d103166f5a9c3c2efafa1b5e59a18a0eac02b76b
KataevYuri <107112301+KataevYuri@users.noreply.github.com>
2022-12-08 16:41:14 +0800

    1

---
+++ Home Work 6/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+
+namespace Home_Work_6
+{
+    internal class Program
+
+    {
+
+        const string FileName = "employes.txt";
+        static string[] data;
+
+        static void Main()
+        {
+            while (true)
+            {
+                Clear();
+
+
+
+                string input = ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        ReadData();
+                        PrintData();
+                        break;
+
+                    case "2":
+                        WriteData();
+                        break;
+
+                    default:
+                        return;
+               }
+            }
+        }
+
+        static private void ReadData()
+        {
+            data = File.ReadAllLines(FileName); 
+        }
+
+        static private void WriteData()
+        {
+            File.WriteAllLines(FileName, data);
+        }
+
+        static private void PrintData()
+        {
+            //File.WriteAllLines(FileName, data);
+        }
+
+    }
+}
